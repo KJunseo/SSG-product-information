ab39dd84688121a1349839eef718e3b46b5f7407
kimjunseo <air.junseo@gmail.com>
2022-06-11 02:51:10 +0900

    project init

---
+++ src/test/java/ssg/product_information/ProductInformationApplicationTests.java
@@ -0,0 +1,14 @@
+package ssg.product_information;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInformationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
